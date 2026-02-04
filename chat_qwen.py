c9f1aac9ba6fcc5c5e8dc33a4952856706e8b1fd
gpu-server <paul.bin0117@gmail.com>
2026-02-04 12:05:25 +0900

    fix: indentation syntax error

--- chat_qwen.py
+++ chat_qwen.py
@@ -28,8 +28,8 @@ def load_model(model_path: str, device: str = "auto"):
     tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
 
     if tokenizer.pad_token is None:
-    tokenizer.pad_token = tokenizer.eos_token
-    
+        tokenizer.pad_token = tokenizer.eos_token
+
     model = AutoModelForCausalLM.from_pretrained(
         model_path,
         torch_dtype=dtype,
